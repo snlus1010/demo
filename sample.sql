232ea06551d57a6648ee28a74cf1cebe99be1d19
snlus1010 <109427322+snlus1010@users.noreply.github.com>
2022-07-17 00:56:47 +0900

    Add files via upload

---
+++ sample.sql
@@ -0,0 +1,49 @@
+# A회사 Sample
+insert into product_info (brand, category, price) values('A', '상의', 11200);
+insert into product_info (brand, category, price) values('A', '아우터', 5500);
+insert into product_info (brand, category, price) values('A', '바지', 4200);
+insert into product_info (brand, category, price) values('A', '스니커즈', 9000);
+insert into product_info (brand, category, price) values('A', '가방', 2000);
+insert into product_info (brand, category, price) values('A', '모자', 1700);
+insert into product_info (brand, category, price) values('A', '양말', 1800);
+insert into product_info (brand, category, price) values('A', '액세서리', 2300);
+
+# B회사 Sample
+insert into product_info (brand, category, price) values('B', '상의', 10500);
+insert into product_info (brand, category, price) values('B', '아우터', 5900);
+insert into product_info (brand, category, price) values('B', '바지', 3800);
+insert into product_info (brand, category, price) values('B', '스니커즈', 9100);
+insert into product_info (brand, category, price) values('B', '가방', 2100);
+insert into product_info (brand, category, price) values('B', '모자', 2000);
+insert into product_info (brand, category, price) values('B', '양말', 2000);
+insert into product_info (brand, category, price) values('B', '액세서리', 2200);
+
+# C회사 Sample
+insert into product_info (brand, category, price) values('C', '상의', 10000);
+insert into product_info (brand, category, price) values('C', '아우터', 6200);
+insert into product_info (brand, category, price) values('C', '바지', 3300);
+insert into product_info (brand, category, price) values('C', '스니커즈', 9200);
+insert into product_info (brand, category, price) values('C', '가방', 2200);
+insert into product_info (brand, category, price) values('C', '모자', 1900);
+insert into product_info (brand, category, price) values('C', '양말', 2200);
+insert into product_info (brand, category, price) values('C', '액세서리', 2100);
+
+# D회사 Sample
+insert into product_info (brand, category, price) values('D', '상의', 10100);
+insert into product_info (brand, category, price) values('D', '아우터', 5100);
+insert into product_info (brand, category, price) values('D', '바지', 3000);
+insert into product_info (brand, category, price) values('D', '스니커즈', 9500);
+insert into product_info (brand, category, price) values('D', '가방', 2500);
+insert into product_info (brand, category, price) values('D', '모자', 1500);
+insert into product_info (brand, category, price) values('D', '양말', 2400);
+insert into product_info (brand, category, price) values('D', '액세서리', 2000);
+
+# E회사 Sample
+insert into product_info (brand, category, price) values('E', '상의', 10700);
+insert into product_info (brand, category, price) values('E', '아우터', 5000);
+insert into product_info (brand, category, price) values('E', '바지', 3800);
+insert into product_info (brand, category, price) values('E', '스니커즈', 9900);
+insert into product_info (brand, category, price) values('E', '가방', 2300);
+insert into product_info (brand, category, price) values('E', '모자', 1800);
+insert into product_info (brand, category, price) values('E', '양말', 2100);
+insert into product_info (brand, category, price) values('E', '액세서리', 2000);
